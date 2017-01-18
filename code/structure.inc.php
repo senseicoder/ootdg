1521cd31f2eab113195c344375a779bd2f01534c
Cédric Girard <cedric@daneel.net>
2017-01-18 08:34:08 +0100

    début des todos, retrait tables

--- code/structure.inc.php
+++ code/structure.inc.php
@@ -9,52 +9,52 @@ class CConfTDG
 #fk potentiellement nulles ? 
 #fusion records sur relation 1-1 entre projet et tâche primaire
 
-CConfTDG::$aTables['tdg_users'] = array(
-	'champs' => array(
-		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
-		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
-	),
-	'index' => array(
-		'lib' => array('type' => COOIndex::UNIQUE, 'champs' => array('lib')),
-	),
-);
+#CConfTDG::$aTables['tdg_users'] = array(
+#	'champs' => array(
+#		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
+#		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
+#	),
+#	'index' => array(
+#		'lib' => array('type' => COOIndex::UNIQUE, 'champs' => array('lib')),
+#	),
+#);
 
-CConfTDG::$aTables['tdg_users_domaines'] = array(
-	'champs' => array(
-		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
-		'id_domaine' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_domaines'),
-		'id_user' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_users'),
-	),
-	'index' => array(
-		'lien' => array('type' => COOIndex::UNIQUE, 'champs' => array('id_domaine', 'id_user')),
-	),
-);
+#CConfTDG::$aTables['tdg_users_domaines'] = array(
+#	'champs' => array(
+#		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
+#		'id_domaine' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_domaines'),
+#		'id_user' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_users'),
+#	),
+#	'index' => array(
+#		'lien' => array('type' => COOIndex::UNIQUE, 'champs' => array('id_domaine', 'id_user')),
+#	),
+#);
 
-CConfTDG::$aTables['tdg_domaines'] = array(
-	'champs' => array(
-		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
-		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
-	),
-	'index' => array(
-		'lib' => array('type' => COOIndex::UNIQUE, 'champs' => array('lib')),
-	),
-);
+#CConfTDG::$aTables['tdg_domaines'] = array(
+#	'champs' => array(
+#		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
+#		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
+#	),
+#	'index' => array(
+#		'lib' => array('type' => COOIndex::UNIQUE, 'champs' => array('lib')),
+#	),
+#);
 
-CConfTDG::$aTables['tdg_users_projets'] = array(
-	'champs' => array(
-		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
-		'id_projet' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_projets'),
-		'id_user' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_users'),
-	),
-	'index' => array(
-		'lien' => array('type' => COOIndex::UNIQUE, 'champs' => array('id_projet', 'id_user')),
-	),
-);
+#CConfTDG::$aTables['tdg_users_projets'] = array(
+#	'champs' => array(
+#		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
+#		'id_projet' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_projets'),
+#		'id_user' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_users'),
+#	),
+#	'index' => array(
+#		'lien' => array('type' => COOIndex::UNIQUE, 'champs' => array('id_projet', 'id_user')),
+#	),
+#);
 
 CConfTDG::$aTables['tdg_projets'] = array(
 	'champs' => array(
 		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
-		'id_domaine' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_domaines'),
+		#'id_domaine' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_domaines'),
 		'id_tache' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_taches'),
 		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
 	),
@@ -74,14 +74,14 @@ CConfTDG::$aTables['tdg_taches'] = array(
 	),
 );
 
-CConfTDG::$aTables['tdg_notes'] = array(
-	'champs' => array(
-		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
-		'id_tache' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_taches'),
-		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
-		'txt' => array('type' => COOTypesChamps::TEXT, 'nature' => COONatureChamps::SAISIE),
-	),
-	'index' => array(
-		'lib' => array('type' => COOIndex::UNIQUE, 'champs' => array('lib')),
-	),
-);
+#CConfTDG::$aTables['tdg_notes'] = array(
+#	'champs' => array(
+#		'id' => array('type' => COOTypesChamps::PRIMARY, 'nature' => COONatureChamps::AUTOINC),
+#		'id_tache' => array('type' => COOTypesChamps::FK, 'nature' => COONatureChamps::FK, 'FK' => 'tdg_taches'),
+#		'lib' => array('type' => COOTypesChamps::STRING, 'nature' => COONatureChamps::SAISIE, 'label' => TRUE),
+#		'txt' => array('type' => COOTypesChamps::TEXT, 'nature' => COONatureChamps::SAISIE),
+#	),
+#	'index' => array(
+#		'lib' => array('type' => COOIndex::UNIQUE, 'champs' => array('lib')),
+#	),
+#);
---
+++ todo.rst
@@ -0,0 +1,23 @@
+Todo
+####
+
+Première itération
+==================
+
+* création de records
+
+Seconde itération
+=================
+
+* gestion de la fusion records 1-1
+
+Itérations suivantes
+====================
+
+* notes
+* fichiers joints
+* univers
+* gestion des droits
+* contextes gtd
+* jounalisation des modifs
+* listings spécifiques intégrant des actions
